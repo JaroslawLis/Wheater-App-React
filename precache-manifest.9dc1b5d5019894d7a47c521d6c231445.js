30ff6c3b78f467aff09a9bdfe305a3cc08903307
JaroslawLis <niejarek@poczta.onet.pl>
2019-05-31 09:28:58 +0200

    Updates

---
+++ precache-manifest.9dc1b5d5019894d7a47c521d6c231445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cd2df522ed507e2e19cc8535fe8144",
+    "url": "/Wheater-App-React/index.html"
+  },
+  {
+    "revision": "8d67754e8f463824170a",
+    "url": "/Wheater-App-React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d267446c725ed3615f55",
+    "url": "/Wheater-App-React/static/js/2.0f9a3c6c.chunk.js"
+  },
+  {
+    "revision": "8d67754e8f463824170a",
+    "url": "/Wheater-App-React/static/js/main.92655f90.chunk.js"
+  },
+  {
+    "revision": "0d9d3708a7152930ebf1",
+    "url": "/Wheater-App-React/static/js/runtime~main.0d32423f.js"
+  }
+]);
